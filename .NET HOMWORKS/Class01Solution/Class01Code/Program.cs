cc5557bd508fd5f25c1b3b26dd8be9e75ae3b4fc
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-03-23 19:52:58 +0100

    Class 1 Homework

---
+++ .NET HOMWORKS/Class01Solution/Class01Code/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Class01Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("*   *   *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine(" *     *");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ReadLine();
+        }
+    }
+}
