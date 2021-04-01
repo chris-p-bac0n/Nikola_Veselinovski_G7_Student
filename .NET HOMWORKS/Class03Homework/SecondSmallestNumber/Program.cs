90f6e8a088d8298a328a14c54383de78e7bdbd86
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-04-01 20:39:07 +0200

    Homework update

---
+++ .NET HOMWORKS/Class03Homework/SecondSmallestNumber/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace SecondSmallestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool successfulNumberConversion = false;
+            bool loopDone = false;
+            int[] arrayWithNumbers = new int[] {};
+
+            do
+            {
+                Console.WriteLine("Please enter a number (Enter N when you're done)");
+                string inputNumber = Console.ReadLine();
+                successfulNumberConversion = int.TryParse(inputNumber, out int number);
+
+                if (inputNumber.ToLower() == "n")
+                {
+                    if (arrayWithNumbers.Length < 2)
+                    {
+                        Console.WriteLine("You must enter atleast 2 numbers for this to work!");
+
+                        continue;
+                    }
+                    
+                    loopDone = true;
+
+                    Console.WriteLine($"Numbers: {PrintArray(arrayWithNumbers)}");
+                    Console.WriteLine($"The second smallest number is: {SecondSmallestNumberCalc(arrayWithNumbers)}");
+
+                    break;
+                }
+                else if (!successfulNumberConversion)
+                {
+                    Console.WriteLine("Invalid input! Please enter a number!");
+                }
+                else
+                {
+                    Array.Resize(ref arrayWithNumbers, arrayWithNumbers.Length + 1);
+                    arrayWithNumbers[arrayWithNumbers.Length - 1] = number;
+                }
+
+            }
+            while (loopDone == false);
+        }
+        static string PrintArray(int[] array)
+        {
+            string printedArrayString = "";
+
+            foreach (int item in array)
+            {
+                if (item == array[(array.Length - 1)])
+                {
+                    printedArrayString += $"{item.ToString()}";
+                }
+                else
+                {
+                    printedArrayString += $"{item.ToString()}, ";
+                }
+                
+            }
+
+            return printedArrayString;
+        }
+        static int SecondSmallestNumberCalc(int[] array)
+        {
+            int first, second = array.Length;
+
+            first = second = int.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < first)
+                {
+                    second = first;
+                    first = array[i];
+                }
+                else if (array[i] < second && array[i] != first)
+                    second = array[i];
+            }
+
+            return second;
+        }
+    }
+}
---
+++ .NET HOMWORKS/Class04Homework/Exercise3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool successfulNumberConversion = false;
+            int n = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                string inputNumber = Console.ReadLine();
+                successfulNumberConversion = int.TryParse(inputNumber, out n);
+
+                if (successfulNumberConversion == false || n < 1)
+                {
+                    Console.WriteLine("Invalid input! Please enter a number bigger than 0");
+                }
+            }
+            while (successfulNumberConversion == false || n < 1);
+
+            Substrings(n);
+
+        }
+
+        static void Substrings(int n)
+        {
+            string welcomeString = "Hello from SEDC Codecademy 2021";
+
+            string newString = welcomeString.Substring(0, n);
+
+            Console.WriteLine($"The first {n} characters are: \"{newString}\" \nAnd the lenth of that string is {newString.Length}");
+        }
+    }
+}
