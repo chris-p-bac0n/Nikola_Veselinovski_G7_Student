cf4176956ffd018fd177d1f90d039b5365352899
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-04-03 00:04:09 +0200

    Bonus Homework

---
+++ .NET HOMWORKS/Class04Homework/CharactersGame/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+
+namespace CharactersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string inputSentance = "";
+            string inputCharacter = "";
+            string inputTryAgainAnswer = "";
+            string inputNumber = "";
+            bool numberValidCheck = false;
+            bool tryAgain = true;
+            int loop1Count = 0;
+            int loop2Count = 0;
+
+            do
+            {
+                if (loop1Count > 0 && tryAgain)
+                {
+                    do
+                    {
+                        Console.WriteLine("Would you like to try again? (Y/N)");
+                        inputTryAgainAnswer = Console.ReadLine();
+
+                        if (inputTryAgainAnswer.ToLower() == "n")
+                        {
+                            tryAgain = false;
+                            break;
+                        }
+                        else if (inputTryAgainAnswer.ToLower() == "y")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    while (ValidateAnswer(inputTryAgainAnswer));
+                }
+
+                if (tryAgain == false)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Please enter a sentance:");
+                inputSentance = Console.ReadLine();
+
+                loop1Count++;
+            }
+
+            while (!ValidateSentance(inputSentance));
+
+            if (tryAgain == true)
+            {
+                do
+                {
+                    if (loop2Count > 0)
+                    {
+                        do
+                        {
+                            Console.WriteLine("Would you like to try again? (Y/N)");
+                            inputTryAgainAnswer = Console.ReadLine();
+
+                            if (inputTryAgainAnswer.ToLower() == "n")
+                            {
+                                tryAgain = false;
+                                break;
+                            }
+                            else if (inputTryAgainAnswer.ToLower() == "y")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+
+                        while (ValidateAnswer(inputTryAgainAnswer));
+                    }
+
+                    if (tryAgain == false)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Please enter a character:");
+                    inputCharacter = Console.ReadLine();
+
+                    loop2Count++;
+                }
+
+                while (!ValidateCharacter(inputCharacter));
+
+                if (tryAgain == true)
+                {
+                    Console.WriteLine("The character is present in the sentence for a total of " + CharacterCount(inputSentance, inputCharacter) + " times!");
+
+                    do
+                    {
+                        Console.WriteLine("Please enter a number: (Must not be larger than the sentence length)");
+                        inputNumber = Console.ReadLine();
+                        numberValidCheck = ValidateNumber(inputNumber, inputSentance);
+
+                        if (numberValidCheck)
+                        {
+                            bool successfulNumberConversion = int.TryParse(inputNumber, out int number);
+                            string sentanceSubStr = inputSentance.Substring(0, number);
+
+                            Console.WriteLine($"The substring of the sentence is: \"{sentanceSubStr}\"");
+                        }
+                    }
+
+                    while (!numberValidCheck);
+                }
+            }         
+        }
+        static bool ValidateSentance(string inputSentance)
+        {
+            bool sentanceValid = false;
+            bool onlyWhiteSpace = false;
+
+            onlyWhiteSpace = String.IsNullOrWhiteSpace(inputSentance);
+            int numberOfLetters = 0;
+
+            for (int i = 0; i < inputSentance.Length; i++)
+            {
+                if (Char.IsLetter(inputSentance, i) == true)
+                {
+                    numberOfLetters += 1;
+                }
+            }
+            if (onlyWhiteSpace == true)
+            {
+                Console.WriteLine("You only entered white space or nothing at all, please enter a sentance!");
+            }
+            else if (numberOfLetters < 8)
+            {
+                Console.WriteLine("Please enter a sentance with at least 8 letters!");
+            }
+            else
+            {
+                sentanceValid = true;
+            }
+
+            return sentanceValid;
+        }
+        static bool ValidateCharacter(string inputCharacter)
+        {
+            bool characterValid = false;
+            bool onlyWhiteSpace = false;
+
+            onlyWhiteSpace = String.IsNullOrWhiteSpace(inputCharacter);
+            int numberOfLetters = 0;
+
+            for (int i = 0; i < inputCharacter.Length; i++)
+            {
+                if (Char.IsLetter(inputCharacter, i) == true)
+                {
+                    numberOfLetters += 1;
+                }
+            }
+            if (onlyWhiteSpace == true)
+            {
+                Console.WriteLine("You only entered white space or nothing at all, please enter a character!");
+            }
+            else if (numberOfLetters > 1)
+            {
+                Console.WriteLine("Please enter only 1 character");
+            }
+            else
+            {
+                characterValid = true;
+            }
+
+            return characterValid;
+        }
+        static bool ValidateAnswer(string inputTryAgainAnswer)
+        {
+            bool answerValid = false;
+
+            if (inputTryAgainAnswer.ToLower() == "y" || inputTryAgainAnswer.ToLower() == "n")
+            {
+                answerValid = true;
+            }
+
+            return answerValid;
+        }
+        static int CharacterCount(string inputSentance, string inputCharacter)
+        {
+            bool successfulCharConversion = char.TryParse(inputCharacter, out char countCharacter);
+
+            int count = 0;
+            foreach (char a in inputSentance)
+                if (a == countCharacter) count++;
+
+            return count;
+        }
+
+        static bool ValidateNumber(string inputNumber, string inputSentance)
+        {
+            bool numberValid = false;            
+            bool successfulNumberConversion = int.TryParse(inputNumber, out int number);
+
+            if (!successfulNumberConversion)
+            {
+                Console.WriteLine("Input is not valid! Must be a number!");
+            }
+            else if (number > inputSentance.Length)
+            {
+                Console.WriteLine("Number must not be larger than the sentence length!");
+            }
+            else
+            {
+                numberValid = true;
+            }
+
+            return numberValid;
+        }
+    }
+}
---
+++ .NET HOMWORKS/Class04Homework/FunWithStrings/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace FunWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentance:");
+            string inputString = Console.ReadLine();
+
+            FunWithStrings(inputString);
+        }
+        static void FunWithStrings(string inputString)
+        {
+            Console.WriteLine(ReverseString(inputString));
+            Console.WriteLine(VowelsCount(inputString));
+            Console.WriteLine(PalindromeCheck(inputString));
+            Console.WriteLine(LargestWord(inputString));
+            Console.WriteLine(SmallestWord(inputString));
+            Console.WriteLine(NumberOfWords(inputString));
+            Console.WriteLine(MostUsedChar(inputString));
+        }
+
+        static string ReverseString(string inputString)
+        {
+            char[] reverseArray = inputString.ToCharArray();
+            Array.Reverse(reverseArray);
+            return new string(reverseArray);
+        }
+        static string VowelsCount(string inputString)
+        {
+            int vowels = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == 'a' || inputString[i] == 'e' || inputString[i] == 'i' || inputString[i] == 'o' || inputString[i] == 'u' || inputString[i] == 'A' || inputString[i] == 'E' || inputString[i] == 'I' || inputString[i] == 'O' || inputString[i] == 'U')
+                {
+                    vowels++;
+                }
+            }
+
+            return $"The total number of vowels in the string is: {vowels}";
+        }
+        static string PalindromeCheck(string inputString)
+        {
+            char[] reverseArray = inputString.ToCharArray();
+            Array.Reverse(reverseArray);
+
+            if (new string(reverseArray) == inputString)
+            {
+                return "The string is a palindrome";
+            }
+            else
+            {
+                return "The string is not a palindrome";
+            }            
+        }
+
+        static string LargestWord(string inputString)
+        {
+            string[] stringArray = inputString.Split(' ');
+
+            var shortString = stringArray[0];
+            var longString = stringArray[0];
+
+            foreach (string a in stringArray)
+            {
+                if (shortString.Length > a.Length)
+                {
+                    shortString = a;
+                }
+                if (longString.Length < a.Length)
+                {
+                    longString = a;
+                }
+            }
+            return $"The largest word is: {longString}";
+
+        }
+        static string SmallestWord(string inputString)
+        {
+            string[] stringArray = inputString.Split(' ');
+                        
+                var shortString = stringArray[0];
+                var longString = stringArray[0];
+
+                foreach (string a in stringArray)
+                {
+                    if (shortString.Length > a.Length)
+                    {
+                        shortString = a;
+                    }
+                    if (longString.Length < a.Length)
+                    {
+                        longString = a;
+                    }
+                }
+                return $"The smallest word is: {shortString}";         
+        }
+        static string NumberOfWords(string inputString)
+        {
+            string[] stringArray = inputString.Split(' '); 
+            
+            return $"The number of words is: {stringArray.Length}";
+        }
+        static string MostUsedChar(string inputString)
+        {
+            int[] charCount = new int[256];
+            int length = inputString.Length;
+            for (int i = 0; i < length; i++)
+            {
+                charCount[inputString[i]]++;
+            }
+            int maxCount = -1;
+            char character = ' ';
+            for (int i = 0; i < length; i++)
+            {
+                if (Char.IsWhiteSpace(inputString[i])) continue;
+
+                if (maxCount < charCount[inputString[i]])
+                {
+                    maxCount = charCount[inputString[i]];
+                    character = inputString[i];
+                }
+            }
+
+            return $"The most used character is: {character} and it occurs {maxCount} times";
+        }    
+    }
+}
