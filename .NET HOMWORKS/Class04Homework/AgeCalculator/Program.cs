8fa0adcae33368db13a50e9fff704bca69b3d4d9
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-04-04 15:25:12 +0200

    new Homework

---
+++ .NET HOMWORKS/Class04Homework/AgeCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool successfulDateConversion = false;
+
+            do
+            {                
+                Console.WriteLine("Please enter your birtday date: (MM/DD/YYYY)");
+                string inputBirthDate = Console.ReadLine();
+                successfulDateConversion = DateTime.TryParse(inputBirthDate, out DateTime birthDate);
+
+                if (!successfulDateConversion)
+                {
+                    Console.WriteLine("Please make sure you entered a correct date format: (MM/DD/YYYY)");
+                    continue;
+                }
+
+                int userAge = AgeCalculator(birthDate);
+                DateTime dateToday = DateTime.Today;
+
+                if (dateToday.AddYears(-userAge) == birthDate)
+                {
+                    Console.WriteLine($"You are {userAge} years old and today is your birthday!");
+                }
+                else if (dateToday.AddYears(-userAge).AddDays(-1) == birthDate)
+                {
+                    Console.WriteLine($"You are {userAge} years old and yesterday was your birthday!");
+                }
+                else if (dateToday.AddYears(-userAge).AddDays(1) == birthDate)
+                {
+                    Console.WriteLine($"You are {userAge} years old and tommorrow is your birthday!");
+                }
+                else 
+                {
+                    Console.WriteLine($"You are {userAge} years old!");
+                }
+            }
+            while (!successfulDateConversion);            
+        }
+        static int AgeCalculator(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate > today.AddYears(-age))
+                age--;
+
+            return age;
+        }
+    }
+}
---
+++ .NET HOMWORKS/Class04Homework/GuessTheSecretNumberGame/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace GuessTheSecretNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 474;
+            int numberGuess = 0;
+            string inputNumberGuess = String.Empty;
+            string inputTryAgainAnswer = String.Empty;
+            bool tryAgain = true;
+            int numberOfTries = 5;
+
+            do
+            {
+                numberOfTries = 5;
+                do
+                {
+                    Console.WriteLine("Guess the secret number!");
+                    inputNumberGuess = Console.ReadLine();
+
+                    if (ValidateNumber(inputNumberGuess))
+                    {
+                        numberOfTries--;
+
+                        numberGuess = int.Parse(inputNumberGuess);
+
+                        NumberGuessing(numberGuess, secretNumber, numberOfTries);
+
+                        if (numberOfTries == 0 && numberGuess != secretNumber)
+                        {
+                            Console.WriteLine("Better luck next time!");
+                        }
+                    }
+                }
+                while (numberOfTries > 0 && numberGuess != secretNumber);
+
+                if (numberGuess != secretNumber)
+                {
+                    do
+                    {
+                        Console.WriteLine("Would you like to try again? (Y/N)");
+                        inputTryAgainAnswer = Console.ReadLine();
+
+                        if (inputTryAgainAnswer.ToLower() == "n")
+                        {
+                            tryAgain = false;
+                            break;
+                        }
+                        else if (inputTryAgainAnswer.ToLower() == "y")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    while (ValidateAnswer(inputTryAgainAnswer));
+                }   
+                else
+                {
+                    tryAgain = false;
+                }
+            }
+            while (tryAgain);
+        }
+        static bool ValidateNumber(string inputNumber)
+        {
+            bool numberValid = false;
+            bool successfulNumberConversion = int.TryParse(inputNumber, out int number);
+
+            if (!successfulNumberConversion)
+            {
+                Console.WriteLine("Input is not valid! Must be a number!");
+            }           
+            else
+            {
+                numberValid = true;
+            }
+
+            return numberValid;
+        }
+        static void NumberGuessing (int inputNumber, int secretNumber, int numberOfTries)
+        {
+            if (inputNumber < secretNumber)
+            {
+                Console.WriteLine("The secret number is greater than the entered number");
+            }
+            else if (inputNumber > secretNumber)
+            {
+                Console.WriteLine("The secret number is less than the entered number");
+            }
+            else 
+            {
+                Console.WriteLine($"Congratulations you guessed the secret number {secretNumber}!");
+            }
+
+            int numberDiff = Math.Abs(secretNumber - inputNumber);
+
+            if (numberDiff >= 50 && numberDiff <= 100)
+            {
+                Console.WriteLine($"You are close to the secret number and have {numberOfTries} number of tries left");
+            }
+            else if (numberDiff >= 1 && numberDiff <= 50)
+            {
+                Console.WriteLine($"You are very close and have {numberOfTries} number of tries left");
+            }            
+        }
+        static bool ValidateAnswer(string inputTryAgainAnswer)
+        {
+            bool answerValid = false;
+
+            if (inputTryAgainAnswer.ToLower() == "y" || inputTryAgainAnswer.ToLower() == "n")
+            {
+                answerValid = true;
+            }
+
+            return answerValid;
+        }
+    }
+}
