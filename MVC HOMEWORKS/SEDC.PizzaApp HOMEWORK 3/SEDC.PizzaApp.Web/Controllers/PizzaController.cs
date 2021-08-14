d8df2f06f179934404e7f17e31d1ff62dbd9a72a
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-08-14 13:13:48 +0200

    homework

---
+++ MVC HOMEWORKS/SEDC.PizzaApp HOMEWORK 3/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models;
+using SEDC.PizzaApp.Web.Models.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDB.Pizzas;
+            List<PizzaViewModel> pizzaMenu = new List<PizzaViewModel>();
+
+            foreach (Pizza pizza in pizzas)
+            {
+                pizzaMenu.Add(PizzaMapper.PizzaToPizzaViewModel(pizza));
+            }
+
+            ViewData.Add("PizzaListTitle", "Pizza List");
+            return View(pizzaMenu);
+        }
+        public IActionResult Details(int id)
+        {
+            if (id > 0)
+            {
+                Pizza foundPizza = StaticDB.Pizzas.SingleOrDefault(x => x.Id == id);
+
+
+                if (foundPizza == null)
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+
+                PizzaViewModel pizza = PizzaMapper.PizzaToPizzaViewModel(foundPizza);
+                ViewBag.Pizza = pizza;
+                ViewData.Add("PizzaDetailsTitle", "Pizza Details Page");
+                return View("~/Views/Pizza/PizzaDetails.cshtml");
+            }
+            else
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ MVC HOMEWORKS/SEDC.PizzaApp HOMEWORK 3/SEDC.PizzaApp.Web/Models/Mapper/PizzaMapper.cs
@@ -0,0 +1,23 @@
+﻿using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Models.Mapper
+{
+    public static class PizzaMapper
+    {
+        public static PizzaViewModel PizzaToPizzaViewModel(Pizza pizza)
+        {
+            return new PizzaViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                Price = pizza.Price,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+        }
+    }
+}
