c7776e3b410d1d46e990c550c45f011ab1ad5d21
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-11-28 20:01:28 +0100

    update

--- SIDE PROJECTS/currencyConverter/src/App.js
+++ SIDE PROJECTS/currencyConverter/src/App.js
@@ -6,10 +6,6 @@ import './index.css';
 import convertImg from "./img/arrow-left-right.svg";
 
 let baseFromCurrency = "USD";
-// let baseToCurrency = "EUR";
-// let conversionRate;
-// let conversionCurrency;
-// let onChangeCurrency;
 let conversionData;
 
 const apiUrl = `https://freecurrencyapi.net/api/v2/latest?apikey=d80323e0-4eec-11ec-8fc7-8b11d8923846&base_currency=${baseFromCurrency}`
@@ -30,10 +26,7 @@ function App() {
     })
       .then((response) => {
         conversionData = response.data.data;
-
         setCurrencyOptions([response.data.query.base_currency, ...Object.keys(conversionData)]);
-
-        console.log("SET");
         setConversionRate(conversionData[toCurrency]);
 
       })
@@ -58,15 +51,15 @@ function App() {
           currency={currencyOptions}
           selectedCurrency={toCurrency}
           onChangeCurrency={e => {
-            console.log("TO");
             setToCurrency(e.target.value);
             setConversionCurrency(e.target.value);
-            console.log("SETRATE");
-            setConversionRate(conversionData[toCurrency]);
-            // setConversionRate(e.target.value);
+            setConversionRate(conversionData[e.target.value]);
           }}
         />
-        <button type="button" className="mx-4 btn btn-primary btn-sm">Convert</button>
+        <button onClick={e => {
+          e.preventDefault() 
+          console.log("hi")}} 
+          type="button" className="mx-4 btn btn-primary btn-sm">Convert</button>
         <div className="d-flex justify-content-center align-items-center">
           <span className="font-weight-bold">=</span>
           <span className="pl-3 font-weight-bold">{conversionRate}</span>
