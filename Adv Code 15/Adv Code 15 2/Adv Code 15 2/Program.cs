1b4f6536ef120a8702b48358d87aa50c4fd2869d
Nikola Veselinovski <nikola.veselinovski@gmail.com>
2021-12-16 13:32:04 +0100

    new code

---
+++ Adv Code 15/Adv Code 15 2/Adv Code 15 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Adv_Code_15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "santaInstructions.txt";
+
+            using var sr = new StreamReader(fileName);
+
+            string instructions = sr.ReadToEnd();
+
+            int deliveryFloor = 0;
+            int instructionIndexForBasement = 0;
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                var woo =  instructions[i] == '(' ? deliveryFloor++ : deliveryFloor--;
+                
+                if(deliveryFloor == -1)
+                {
+                    instructionIndexForBasement = i;
+                    break;
+                }
+            }
+
+            
+
+            Console.WriteLine($"The char index where santa goes to the basement is {instructionIndexForBasement + 1}");
+        }
+    }
+}
---
+++ Adv Code 15/Adv Code 15 3/Adv Code 15 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Adv_Code_15_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var fileName = "listOfDimensions.txt";
+
+            //using var sr = new StreamReader(fileName);
+
+            //string line = String.Empty;
+
+            //int totalNumberOfSqrFeet = 0;
+
+            //while ((line = sr.ReadLine()) != null)
+            //{
+            //    var dimensions = line.Split('x').Select(Int32.Parse).ToList();
+            //    int l = dimensions[0];
+            //    int w = dimensions[1];
+            //    int h = dimensions[2];                
+
+            //    int[] smallestSideCalc = { (l * w), (w * h), (h * l) };
+            //    int smallestSideSqrFeet = smallestSideCalc.Min();
+
+            //    int totalSqrFeetOfPaper = 2 * l * w + 2 * w * h + 2 * h * l + smallestSideSqrFeet;
+            //    totalNumberOfSqrFeet += totalSqrFeetOfPaper;
+            //}
+
+            //Console.WriteLine(totalNumberOfSqrFeet);
+
+            var fileName = "listOfDimensions.txt";
+
+            var dimmensionsArr = File.ReadAllLines(fileName);
+
+            var result = dimmensionsArr
+                .Select(x => x.Split('x'))
+                .Select(x => new List<int> { int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2]) })
+                .Select(x =>
+                {
+                    int[] smallestSideCalc = { (x[0] * x[1]), (x[1] * x[2]), (x[2] * x[0]) };
+
+                    return 2 * x[0] * x[1] + 2 * x[1] * x[2] + 2 * x[2] * x[0] + smallestSideCalc.Min();
+
+                }).Sum();
+        }
+    }
+}
